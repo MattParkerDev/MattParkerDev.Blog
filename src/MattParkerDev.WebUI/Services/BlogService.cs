db7b2ff9c4945e131abb149285e2e9d2ac43493b
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-02-17 12:07:05 +1000

    Create blog.md

--- src/MattParkerDev.WebUI/Services/BlogService.cs
+++ src/MattParkerDev.WebUI/Services/BlogService.cs
@@ -15,6 +15,7 @@ public static class BlogService
 			PublishedDate = new DateTimeOffset(2023, 08, 1, 9, 0, 0, TimeZoneOffset),
 			Slug = "my-first-blog",
 			Title = "My First Blog!",
+			Keywords = [],
 			Description = "A demo blog.",
 			PreviewImageUrl = "./_blogs/my-first-blog/image.webp"
 		},
@@ -25,6 +26,7 @@ public static class BlogService
 			PublishedDate = new DateTimeOffset(2023, 10, 01, 8, 0, 0, TimeZoneOffset),
 			Slug = "expecting-professionalism",
 			Title = "Expecting Professionalism",
+			Keywords = [],
 			Description = "I am your CTO, and these are my expectations of you.",
 			PreviewImageUrl = "./_blogs/expecting-professionalism/image.webp"
 		},
@@ -35,6 +37,7 @@ public static class BlogService
 			PublishedDate = new DateTimeOffset(2023, 10, 08, 14, 10, 0, TimeZoneOffset),
 			Slug = "componentizing-image-format-fallback-blazor",
 			Title = "Componentizing Image Format Fallback in Blazor",
+			Keywords = [],
 			Description = "",
 			PreviewImageIcon = "./img/blazor.svg"
 		},
@@ -45,6 +48,7 @@ public static class BlogService
 			PublishedDate = new DateTimeOffset(2024, 03, 15, 18, 01, 0, TimeZoneOffset),
 			Slug = "efcore-health-checks",
 			Title = "Entity Framework Core (EF Core) Health Checks",
+			Keywords = [],
 			Description = "",
 			PreviewImageUrl = "./_blogs/efcore-health-checks/health-check.webp"
 		},
@@ -55,8 +59,20 @@ public static class BlogService
 			PublishedDate = new DateTimeOffset(2024, 10, 13, 23, 50, 0, TimeZoneOffset),
 			Slug = "azure-managed-identity-postgres-aspnetcore",
 			Title = "ASP.NET Core: Connecting to an Azure PostgreSQL Database with Managed Identity",
+			Keywords = ["C#", "EF Core", "Entity Framework", "Azure", "PostgreSQL", "Managed Identity", "ASP.NET Core"],
 			Description = "",
 			PreviewImageIcon = "./_blogs/azure-managed-identity-postgres-aspnetcore/azure.svg"
+		},
+		new()
+		{
+			Sequence = 5,
+			IsPublished = true,
+			PublishedDate = new DateTimeOffset(2025, 02, 18, 12, 13, 0, TimeZoneOffset),
+			Slug = "windows-program-open-terminal-from-explorer",
+			Title = "Creating a Windows Program to Open a Terminal from Explorer",
+			Keywords = ["Windows", "Explorer", "Terminal", "C#", "Win32", "COM Interface"],
+			Description = "",
+			PreviewImageIcon = "./_blogs/windows-program-open-terminal-from-explorer/windows-explorer.svg"
 		}
 	};
 
