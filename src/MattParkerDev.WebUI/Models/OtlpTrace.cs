589fdc5831c5f26644036c8635cf12e6300d4cec
Matt Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-04-28 12:49:43 +1000

    asdf

---
+++ src/MattParkerDev.WebUI/Models/OtlpTrace.cs
@@ -0,0 +1,20 @@
+﻿using MattParkerDev.WebUI.Services;
+
+namespace MattParkerDev.WebUI.Models;
+
+public class OtlpTrace
+{
+	public required DateTimeOffset StartTime { get; set; }
+	public required DateTimeOffset? EndTime { get; set; }
+	public required TimeSpan? FinalDuration { get; set; }
+	public required bool Finished { get; set; }
+	public required string Name { get; set; }
+
+	public TimeSpan ElapsedDuration => ((EndTime ?? CustomTimeProvider.Custom.GetLocalNow()) - StartTime);
+	public double ElapsedDurationSeconds => ElapsedDuration.TotalSeconds;
+	public double PercentageOfTimeline => Math.Min(ElapsedDurationSeconds / 30 * 100, 100);
+
+	public TimeSpan DurationSinceFinished => CustomTimeProvider.Custom.GetLocalNow() - EndTime ?? TimeSpan.Zero;
+	public double DurationSinceFinishedSeconds => DurationSinceFinished.TotalSeconds;
+	public double PercentageOfTimelineSinceFinished => Math.Min(DurationSinceFinishedSeconds / 30 * 100, 100);
+}
---
+++ src/MattParkerDev.WebUI/Services/CustomTimeProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace MattParkerDev.WebUI.Services;
+
+public class CustomTimeProvider : TimeProvider
+{
+	public static CustomTimeProvider Custom { get; } = new CustomTimeProvider();
+	public bool Paused { get; private set; }
+	private DateTimeOffset? PausedAt { get; set; }
+	public override DateTimeOffset GetUtcNow()
+	{
+		return Paused ? PausedAt!.Value : base.GetUtcNow();
+	}
+
+	public void TogglePause()
+	{
+		if (Paused)
+		{
+			Resume();
+		}
+		else
+		{
+			Pause();
+		}
+	}
+	public void Pause()
+	{
+		PausedAt = GetUtcNow();
+		Paused = true;
+	}
+
+	public void Resume()
+	{
+		Paused = false;
+		PausedAt = null;
+	}
+}
